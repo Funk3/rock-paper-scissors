95529ced1d0571569a182e776540a0e184e3494d
Zak Johansson <zak.johansson@gmail.com>
2022-01-24 14:07:06 -0800

    created new function

--- javascript.js
+++ javascript.js
@@ -17,24 +17,45 @@ const computerSelection = computerPlay();
 
 /* single round function that compares the two inputs and gives a statement according to the conditions */
 function singleRound() {
+    userBalance = 0;
+    computerBalance = 0;
     if (userSelection == "paper" && computerSelection == "rock") {
-        return ("You beat the computer!");
+        userBalance += 1;
+        return (`You beat the computer! Paper beats rock. Player: ${userBalance} Computer: ${computerBalance}`);
     }   else if (userSelection == "scissors" && computerSelection == "paper") {
-        return ("You beat the computer!");
+        userBalance += 1;
+        return (`You beat the computer! Scissors beats paper. ${userBalance} Computer: ${computerBalance}`);
     }   else if (userSelection == "rock" && computerSelection == "scissors") {
-        return ("You beat the computer!");
+        userBalance += 1;
+        return (`You beat the computer! Rock beats scissors. ${userBalance} Computer: ${computerBalance}`);
     }   else if (userSelection == "rock"&& computerSelection == "paper") {
-        return ("You lost, please try again");
+        computerBalance += 1;
+        return (`You lost, paper beats rock. ${userBalance} Computer: ${computerBalance}`);
     }   else if (userSelection == "paper" && computerSelection == "scissors") {
-        return ("You lost, please try again");
+        computerBalance += 1;
+        return (`You lost, scissors beats paper. ${userBalance} Computer: ${computerBalance}`);
     }   else if (userSelection == "scissors" && computerSelection == "rock") {
-        return ("You lost, please try again");
+        computerBalance += 1;
+        return (`You lost, rock beats scissors. ${userBalance} Computer: ${computerBalance}`);
     }   else if (userSelection === computerSelection) {
-        return ("It's a tie. Please try again");
+        return ("It's a tie. Please try again. ${userBalance} Computer: ${computerBalance}");
     }
 }
 
 /* automatically runs the functions when you load the page */
-console.log(singleRound());
 
+/* function that plays the game 5 times and keeps a counter. When the counter reaches 5 you win or lose */
 
+function bestRound() {
+    userBalance = 0;
+    computerBalance = 0;
+    singleRound();
+        if (userBalance == 5) {
+            return ("Congratulations, you won!");
+        } else if (computerBalance == 5) {
+            return ("You lost. reload and play again.")
+        }
+
+}
+
+console.log(bestRound());
