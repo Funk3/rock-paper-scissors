ff331d96bafbf46340f2ee325174938904717009
Zak Johansson <zak.johansson@gmail.com>
2022-01-24 12:24:10 -0800

    fixed the singleRound function

--- javascript.js
+++ javascript.js
@@ -17,16 +17,24 @@ const computerSelection = computerPlay();
 
 /* single round function that compares the two inputs and gives a statement according to the conditions */
 function singleRound() {
-    if (userSelection > computerSelection) {
+    if (userSelection == "paper" && computerSelection == "rock") {
         return ("You beat the computer!");
-    } else if (userSelection === computerSelection) {
+    }   else if (userSelection == "scissors" && computerSelection == "paper") {
+        return ("You beat the computer!");
+    }   else if (userSelection == "rock" && computerSelection == "scissors") {
+        return ("You beat the computer!");
+    }   else if (userSelection == "rock"&& computerSelection == "paper") {
+        return ("You lost, please try again");
+    }   else if (userSelection == "paper" && computerSelection == "scissors") {
+        return ("You lost, please try again");
+    }   else if (userSelection == "scissors" && computerSelection == "rock") {
+        return ("You lost, please try again");
+    }   else if (userSelection === computerSelection) {
         return ("It's a tie. Please try again");
-    } else if (userSelection < computerSelection) {
-        return("You lost. Please try again!");
     }
 }
 
 /* automatically runs the functions when you load the page */
-console.log(singleRound(userSelection, computerSelection));
+console.log(singleRound());
 
 
