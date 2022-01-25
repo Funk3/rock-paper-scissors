6c48d1366eb1cbe2bfd3303d59e8e7c300f92454
Zak Johansson <zak.johansson@gmail.com>
2022-01-25 11:47:55 -0800

    formatting

--- javascript.js
+++ javascript.js
@@ -20,22 +20,17 @@ function singleRound() {
         return (`You beat the computer! Scissors beats paper. ${userBalance} Computer: ${computerBalance}`);
     }   else if (userSelection == "rock" && computerSelection == "scissors") {
         userBalance++;
-        
         return (`You beat the computer! Rock beats scissors. ${userBalance} Computer: ${computerBalance}`);
     }   else if (userSelection == "rock"&& computerSelection == "paper") {
         computerBalance++;
-        
         return (`You lost, paper beats rock. ${userBalance} Computer: ${computerBalance}`);
     }   else if (userSelection == "paper" && computerSelection == "scissors") {
         computerBalance++;
-        
         return (`You lost, scissors beats paper. ${userBalance} Computer: ${computerBalance}`);
     }   else if (userSelection == "scissors" && computerSelection == "rock") {
         computerBalance++;
-        
         return (`You lost, rock beats scissors. ${userBalance} Computer: ${computerBalance}`);
     }   else if (userSelection == computerSelection) {
-        
         return (`It's a tie. Please try again. ${userBalance} Computer: ${computerBalance}`);
     }
 }
