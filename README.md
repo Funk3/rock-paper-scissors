5655c1b1e42ec8dea6a7f2e145bc38b469d87ce5
Zak Johansson <zak.johansson@gmail.com>
2022-02-09 16:51:55 -0800

    completed rock paper scissors update

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 a javascript project assigned by theodinproject.org
 
 Using javascript to create an interactive program where you play rock paper scissors.
+
+image credits:
+
+Image by <a href="https://pixabay.com/users/clker-free-vector-images-3736/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=296855">Clker-Free-Vector-Images</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=296855">Pixabay</a>
+Image by <a href="https://pixabay.com/users/clker-free-vector-images-3736/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=296854">Clker-Free-Vector-Images</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=296854">Pixabay</a>
+Image by <a href="https://pixabay.com/users/clker-free-vector-images-3736/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=296853">Clker-Free-Vector-Images</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=296853">Pixabay</a>
+
--- javascript.js
+++ javascript.js
@@ -1,48 +1,86 @@
+const game = ["rock", "paper", "scissors"];
+
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
 
-const game = ["rock", "scissors", "paper"];
+const results = document.querySelector('#results');
+const win = document.createElement('div');
+
+const playerCounter = document.querySelector('.playercounter');
+const computerCounter = document.querySelector('.computercounter');
+
+rock.addEventListener('click', playRound);
+paper.addEventListener('click', playRound);
+scissors.addEventListener('click', playRound);
+
 let userBalance = 0;
 let computerBalance = 0;
 
-function singleRound() {
+function playRound(e) {
+
     const userSelection = userPlay();
     const computerSelection = computerPlay();
-    
+
+    //when clicking on a button, it returns the value to be used in the variable userSelection
     function userPlay() {
-        rock.addEventListener("click", console.log("rock"));
-        paper.addEventListener("click", console.log('paper'));
-        scissors.addEventListener("click", console.log('scissors'));
-    }
-    
+        if (e.target.id == "rock"){
+            return(game[0]);
+        } else if (e.target.id== 'paper') {
+            return game[1];
+        } else if (e.target.id == 'scissors') {
+            return game [2];
+        }
+    };
+
     function computerPlay() {
-        return game[Math.floor(Math.random()*game.length)];
-    }
+    return game[Math.floor(Math.random()*game.length)];
+    };
 
-    if (userSelection == "paper" && computerSelection == "rock") {
-        userBalance++;
-        return (`You beat the computer! Paper beats rock. Player: ${userBalance} Computer: ${computerBalance}`);
-    }   else if (userSelection == "scissors" && computerSelection == "paper") {
-        userBalance++;
-        return (`You beat the computer! Scissors beats paper. ${userBalance} Computer: ${computerBalance}`);
-    }   else if (userSelection == "rock" && computerSelection == "scissors") {
-        userBalance++;
-        return (`You beat the computer! Rock beats scissors. ${userBalance} Computer: ${computerBalance}`);
-    }   else if (userSelection == "rock"&& computerSelection == "paper") {
-        computerBalance++;
-        return (`You lost, paper beats rock. ${userBalance} Computer: ${computerBalance}`);
-    }   else if (userSelection == "paper" && computerSelection == "scissors") {
-        computerBalance++;
-        return (`You lost, scissors beats paper. ${userBalance} Computer: ${computerBalance}`);
-    }   else if (userSelection == "scissors" && computerSelection == "rock") {
-        computerBalance++;
-        return (`You lost, rock beats scissors. ${userBalance} Computer: ${computerBalance}`);
-    }   else if (userSelection == computerSelection) {
-        return (`It's a tie. Please try again. ${userBalance} Computer: ${computerBalance}`);
-    }
-}
-
+    win.classList.add('win');
+    results.appendChild(win);
     
+    if (userSelection == game[1] && computerSelection == game[0]) {
+            userBalance++;
+            playerCounter.textContent= `${userBalance}`;
+            win.textContent= `You win! Paper beats rock.`;
+    }   else if (userSelection == game[2] && computerSelection == game[1]) {
+            userBalance++;
+            playerCounter.textContent = `${userBalance}`;
+            win.textContent= `You win! Scissors beats paper.`;
+    }   else if (userSelection == game[0] && computerSelection == game[2]) {
+            userBalance++;
+            playerCounter.textContent= `${userBalance}`;
+            win.textContent= `You win! Rock beats scissors.`;
+    }   else if (userSelection == game [0] && computerSelection == game[1]) {
+            computerBalance++;
+            computerCounter.textContent= `${computerBalance}`;
+            win.textContent= `You lost, paper beats rock.`;
+    }   else if (userSelection == game [1] && computerSelection == game[2]) {
+            computerBalance++;
+            computerCounter.textContent= `${computerBalance}`;
+            win.textContent= `You lost, scissors beats paper.`;
+    }   else if (userSelection == game[2] && computerSelection == game[0]) {
+            computerBalance++;
+            computerCounter.textContent= `${computerBalance}`;
+            win.textContent= `You lost, rock beats scissors.`;
+    }   else if (userSelection == computerSelection) {
+            win.textContent= `It's a tie. Please try again.`;
+        } 
+        
+        if ((userBalance == 5) == true){
+            win.textContent = `Congrats! You have beat the computer!`;
+            userBalance = 0;
+            computerBalance = 0;
+            playerCounter.textContent= `${userBalance}`;
+            computerCounter.textContent= `${computerBalance}`;
+        } else if ((computerBalance == 5) == true){
+            win.textContent = `Sorry, SKYNET took over. Try again in... a different life?`;
+            userBalance = 0;
+            computerBalance = 0;
+            playerCounter.textContent= `${userBalance}`;
+            computerCounter.textContent= `${computerBalance}`;
+        }
+}
 
 
